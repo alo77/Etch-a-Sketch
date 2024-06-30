987bf9122170e176ac26d4c9d9354739eca9fd8a
alo77 <110139636+alo77@users.noreply.github.com>
2024-06-30 13:34:46 -0500

    Update script.js
    size, clear, color const; color let

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const container = document.querySelector("#grid-container");
+const changeSize = document.querySelector("#change-size");
+const clearSketch = document.querySelector("#clear-sketch");
+const blackColor = document.querySelector("#black-color");
+const shadeColor = document.querySelector("#shade-color");
+const rgbColor = document.querySelector("#rgb-color");
 
 let gridSize = 16;
+let color = "black";
+let shade = false;
+let rgb = false;
